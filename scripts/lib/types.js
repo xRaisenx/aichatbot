fbf9d66aba676dfd0e263702a4645ea55ecd2a61
xRaisenx <jpm.onestop@gmail.com>
2025-05-23 12:27:12 -0700

    Delete scripts directory
    Signed-off-by: xRaisenx <jpm.onestop@gmail.com>

--- scripts/lib/types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// lib/types.ts
-Object.defineProperty(exports, "__esModule", { value: true });
--- scripts/scripts/testEnv.cjs
+++
@@ -1,9 +0,0 @@
-// D:\PB_NEW5\dist_sim\scripts\testEnv.cjs
-const { config } = require('dotenv');
-const path = require('path');
-
-config({ path: path.resolve(__dirname, '.env') });
-
-console.log('GEMINI_API_KEY:', process.env.GEMINI_API_KEY);
-console.log('UPSTASH_VECTOR_REST_URL:', process.env.UPSTASH_VECTOR_REST_URL);
-console.log('UPSTASH_VECTOR_REST_TOKEN:', process.env.UPSTASH_VECTOR_REST_TOKEN);
